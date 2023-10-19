9ee1954115aafe33e3c0fd02d2ef860adc084e04
Robin Gögge <r.goegge@gmail.com>
2023-10-19 23:33:04 +0200

    interferenceaware: add cpu and block IO scoring
    Signed-off-by: Robin Gögge <r.goegge@gmail.com>

--- cmd/scheduler/main.go
+++ cmd/scheduler/main.go
@@ -24,15 +24,8 @@ import (
 	"k8s.io/component-base/logs"
 	"k8s.io/kubernetes/cmd/kube-scheduler/app"
 
-	"sigs.k8s.io/scheduler-plugins/pkg/capacityscheduling"
-	"sigs.k8s.io/scheduler-plugins/pkg/coscheduling"
-	"sigs.k8s.io/scheduler-plugins/pkg/crossnodepreemption"
-	"sigs.k8s.io/scheduler-plugins/pkg/noderesources"
-	"sigs.k8s.io/scheduler-plugins/pkg/noderesourcetopology"
-	"sigs.k8s.io/scheduler-plugins/pkg/podstate"
-	"sigs.k8s.io/scheduler-plugins/pkg/qos"
-	"sigs.k8s.io/scheduler-plugins/pkg/trimaran/loadvariationriskbalancing"
-	"sigs.k8s.io/scheduler-plugins/pkg/trimaran/targetloadpacking"
+	"sigs.k8s.io/scheduler-plugins/pkg/interferenceaware"
+
 	// Ensure scheme package is initialized.
 	_ "sigs.k8s.io/scheduler-plugins/pkg/apis/config/scheme"
 )
@@ -44,16 +37,19 @@ func main() {
 	// Later they can consist of scheduler profile(s) and hence
 	// used by various kinds of workloads.
 	command := app.NewSchedulerCommand(
-		app.WithPlugin(capacityscheduling.Name, capacityscheduling.New),
-		app.WithPlugin(coscheduling.Name, coscheduling.New),
-		app.WithPlugin(loadvariationriskbalancing.Name, loadvariationriskbalancing.New),
-		app.WithPlugin(noderesources.AllocatableName, noderesources.NewAllocatable),
-		app.WithPlugin(noderesourcetopology.Name, noderesourcetopology.New),
-		app.WithPlugin(targetloadpacking.Name, targetloadpacking.New),
+		// app.WithPlugin(capacityscheduling.Name, capacityscheduling.New),
+		// app.WithPlugin(coscheduling.Name, coscheduling.New),
+		// app.WithPlugin(loadvariationriskbalancing.Name, loadvariationriskbalancing.New),
+		// app.WithPlugin(noderesources.AllocatableName, noderesources.NewAllocatable),
+		// app.WithPlugin(noderesourcetopology.Name, noderesourcetopology.New),
+		// app.WithPlugin(targetloadpacking.Name, targetloadpacking.New),
 		// Sample plugins below.
-		app.WithPlugin(crossnodepreemption.Name, crossnodepreemption.New),
-		app.WithPlugin(podstate.Name, podstate.New),
-		app.WithPlugin(qos.Name, qos.New),
+		// app.WithPlugin(crossnodepreemption.Name, crossnodepreemption.New),
+		// app.WithPlugin(podstate.Name, podstate.New),
+		// app.WithPlugin(qos.Name, qos.New),
+		// ma thesis plugins below.
+		app.WithPlugin(interferenceaware.InterferenceCPUName, interferenceaware.NewInterferenceCPU),
+		app.WithPlugin(interferenceaware.InterferenceBIOName, interferenceaware.NewInterferenceBIO),
 	)
 
 	// TODO: once we switch everything over to Cobra commands, we can go back to calling
